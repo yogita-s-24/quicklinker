fc7799d1a20a5e1698d4785b0246bdbcf69fb427
yogita-s-24 <yogitashete24@gmail.com>
2023-11-17 21:20:21 +0530

    ref : navbar csss

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -3,26 +3,26 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
     return (
-        <div>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary sticky-top shadow">
+        <div className='sticky-top'>
+            <nav className="navbar navbar-expand-lg shadow" style={{backgroundColor: "#845EC2"}}>
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">🔗QuickLinker</Link>
+                    <Link className="navbar-brand text-light fw-bold" to="/">🔗QuickLinker</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mx-auto me-auto mb-2 mb-lg-0">
                             <li className="nav-item me-5 fw-bold">
-                                <Link className="nav-link" aria-current="page" to="/">Home</Link>
+                                <Link className="nav-link text-light" aria-current="page" to="/">Home</Link>
                             </li>
                             <li className="nav-item me-5 fw-bold">
-                                <Link className="nav-link" aria-current="page" to="/about">About</Link>
+                                <Link className="nav-link text-light" aria-current="page" to="/about">About</Link>
                             </li>
                             <li className="nav-item me-5 fw-bold">
-                                <Link className="nav-link" aria-current="page" to="/login">Login</Link>
+                                <Link className="nav-link text-light" aria-current="page" to="/login">Login</Link>
                             </li>
                             <li className="nav-item me-5 fw-bold">
-                                <Link className="nav-link" aria-current="page" to="/signin">Signin</Link>
+                                <Link className="nav-link text-light" aria-current="page" to="/signin">Signup</Link>
                             </li>
 
 
@@ -32,7 +32,7 @@ function Navbar() {
                             <ul className="navbar-nav mx-auto me-auto mb-2 mb-lg-0">
 
                                 <li className="nav-item me-3 fw-bold">
-                                    <Link className="nav-link" aria-current="page" to="/user">User!</Link>
+                                    <Link className="nav-link text-light" aria-current="page" to="/user">User!</Link>
                                 </li>
                             </ul>
                         </form>
